b9a752b3985a7a6d682a8771fb6e21c48d5b87ef
KseniaSadrina <caysebix@gmail.com>
2019-02-19 00:56:23 +0200

    Added enemy patrol

--- ArrowGame/Assets/Scripts/Controllers/EnemyController.cs
+++ ArrowGame/Assets/Scripts/Controllers/EnemyController.cs
@@ -8,16 +8,22 @@ public class EnemyController : MonoBehaviour {
 
 	public float lookRadius = 10f;
 
+
 	Transform target;
 	NavMeshAgent agent;
+	bool chasingTarget = false;
+	// Patrol positions 
 
-	// Use this for initialization
+	public Transform position1;
+	public Transform position2;
+	public Transform position3;
+	public Transform position4;
+
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
 		target = PlayerManager.instance.player.transform;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		float distance = Vector3.Distance(target.position, transform.position);
 		if (distance <= lookRadius)
@@ -26,6 +32,7 @@ public class EnemyController : MonoBehaviour {
 
 			if (distance <= agent.stoppingDistance)
 			{
+				chasingTarget = true;
 				// Attack the target
 				FaceTarget();
 			}
@@ -44,5 +51,25 @@ public class EnemyController : MonoBehaviour {
 		Gizmos.color = Color.red;
 		Gizmos.DrawWireSphere(transform.position, lookRadius);
 		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (chasingTarget) return;
+		switch (other.tag)
+		{
+			case "1":
+				agent.SetDestination(position2.position);
+				break;
+			case "2":
+				agent.SetDestination(position3.position);
+				break;
+			case "3":
+				agent.SetDestination(position4.position);
+				break;
+			case "4":
+				agent.SetDestination(position1.position);
+				break;
+		}
 	}
 }
