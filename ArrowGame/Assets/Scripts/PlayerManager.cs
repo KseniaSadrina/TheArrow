9fe0362e356399e1b3ccbff5a00a86014f63696a
KseniaSadrina <caysebix@gmail.com>
2019-02-19 00:13:23 +0200

    Added enemy ai
    chasing the charecter + stats

---
+++ ArrowGame/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	#region Singleton
+
+	public static PlayerManager instance;
+
+	private void Awake()
+	{
+		if (instance != null) return;
+		instance = this;
+	}
+
+	#endregion
+
+	public GameObject player;
+}
