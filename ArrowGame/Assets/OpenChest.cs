b02edd38abcafbbb46688e73d422e552a7941185
KseniaSadrina <caysebix@gmail.com>
2019-02-22 01:18:42 +0200

    added tressures to scene
    with open animation triggered on right player mouse click (on the item)

---
+++ ArrowGame/Assets/OpenChest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : Interactable {
+
+	public Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	public override void Interact()
+	{
+		base.Interact();
+		anim.Play("OpenChest");
+
+	}
+}
